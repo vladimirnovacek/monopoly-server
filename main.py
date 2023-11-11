3dac28caeb19a1434e8ac718bf0533bca4574280
vladimirnovacek <novacek.vladimir@gmail.com>
2023-11-11 22:44:08 +0100

    main method updated

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from twisted.internet import reactor
 
 import config
+import data_parser
+import game_data
 import message_factory
 from server import ServerFactory
 
 
 def start_server():
-    factory = ServerFactory(message_factory.MessageFactory())
+    gdata = game_data.GameData()
+    mess_factory = message_factory.MessageFactory()
+    parser = data_parser.MessageParser(gdata, mess_factory)
+    factory = ServerFactory(mess_factory, parser)
     reactor.listenTCP(config.listen_port, factory)
     reactor.run()
 
