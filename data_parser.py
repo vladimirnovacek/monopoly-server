869d6f35fb498e0bd9bbc4fb1ef2e15eaa72e48a
vladimirnovacek <novacek.vladimir@gmail.com>
2023-11-13 11:43:37 +0100

    Improved acquiring player_id, improved adding a new player

--- data_parser.py
+++ data_parser.py
@@ -1,6 +1,6 @@
 
 import pickle
-from typing import Callable
+from typing import Any, Callable, Literal, Optional, TypedDict
 
 import uuid
 
@@ -9,22 +9,33 @@ import server
 from game_data import GameData
 
 
+class ClientMessage(TypedDict):
+    my_uuid: uuid.UUID
+    action: str
+    parameters: dict
+
+
+class ServerMessage(TypedDict):
+    section: Literal["fields", "players", "misc"]
+    item: str | uuid.UUID
+    attribute: Optional[str]
+    value: Any
+
+
 class MessageParser:
     def __init__(self, game_data: GameData, message_factory: MessageFactory):
         self.game_data: GameData = game_data
         self.message: MessageFactory = message_factory
-        self.network: server.Server | None = None
+        self.network: server.ServerFactory | None = None
 
     def get_initial_message(self, player_uuid: uuid.UUID) -> bytes:
         """
         Generates the initial message for the given player containing all necessary data from the game data.
         The message is already pickled and ready to be sent.
-
-        Args:
-            player_uuid (uuid.UUID): The UUID of the player.
-
-        Returns:
-            bytes: The initial message as bytes.
+        :param player_uuid: The UUID of the player.
+        :type player_uuid: uuid.UUID
+        :return: The initial message as bytes.
+        :rtype: bytes
         """
         for record in self.game_data.get_all_for_player(player_uuid):
             self.message.add(**record)
@@ -33,13 +44,11 @@ class MessageParser:
     @staticmethod
     def retype(method: Callable) -> Callable:
         """
-        A decorator that changes the input type of a method.
-
-        Parameters:
-            method (Callable): The method to be decorated.
-
-        Returns:
-            Callable: The decorated method.
+        A decorator that changes the return type of a method.
+        :param method: The method to be decorated.
+        :type method: typing.Callable
+        :return: The decorated method.
+        :rtype: typing.Callable
         """
         def wrapper(self, data):
             if type(data) == bytes:
@@ -50,14 +59,16 @@ class MessageParser:
         return wrapper
 
     @retype
-    def parse(self, message):
+    def parse(self, message: ClientMessage | bytes) -> None:
         """
         Parses the given message and updates the game data.
-        :param message:
-        :return:
+        :param message: The message to be parsed.
+        :type message: dict | bytes
+        :return: None
+        :rtype: None
         """
-        if message["action"] == "add_player":
-            self._add_player(message["my_uuid"], message["parameters"]["my_id"])
+        if self.network.server_uuid == message["my_uuid"]:
+            self._add_player(message["parameters"]["player_uuid"], message["parameters"]["player_id"])
 
     def _add_player(self, player_uuid: uuid.UUID, player_id: int):
         self.game_data.add_player(player_uuid, player_id)
--- server.py
+++ server.py
@@ -11,16 +11,19 @@ import interfaces
 class Server(Protocol):
     factory: "ServerFactory"
     uuid: uuid.UUID
+    player_id: int
 
     def connectionMade(self):
-        if len(self.factory.connected_clients) >= 4:
+        if not self.factory.available_ids:
             self.transport.loseConnection()
             return
+        else:
+            self.player_id = self.factory.get_id()
         self.uuid = uuid.uuid4()
         self.factory.connected_clients[self.uuid] = self
         dic = {
-            "my_uuid": self.uuid, "action": "add_player",
-            "parameters": {"my_id": len(self.factory.connected_clients) - 1}
+            "my_uuid": self.factory.server_uuid, "action": "add_player",
+            "parameters": {"player_uuid": self.uuid, "player_id": self.player_id}
         }
         self.factory.parser.parse(dic)
         message = self.factory.parser.get_initial_message(self.uuid)
@@ -31,10 +34,7 @@ class Server(Protocol):
             del self.factory.connected_clients[self.uuid]
 
     def dataReceived(self, data: bytes):
-        self.factory.message.add(
-            section="players", item=0, attribute="name", value="Player 1"
-        )
-        self.transport.write(self.factory.message.get())
+        print("Data received: ", pickle.loads(data))
 
     def broadcast(self, data: bytes):
         message = f"{self.uuid}: {pickle.loads(data)}"
@@ -48,6 +48,19 @@ class ServerFactory(Factory):
     protocol = Server
 
     def __init__(self, message_factory: interfaces.MessageFactory, parser: interfaces.Parser):
+        self.server_uuid = uuid.uuid4()
         self.message: interfaces.MessageFactory = message_factory
         self.parser: interfaces.Parser = parser
+        self.parser.network = self
         self.connected_clients = dict()
+        self.available_ids = set(range(1, 5))
+
+    def get_id(self) -> int:
+        """
+        Gets an available player ID.
+        :return: The player ID.
+        :rtype: int
+        """
+        player_id = min(self.available_ids)
+        self.available_ids.remove(player_id)
+        return player_id
