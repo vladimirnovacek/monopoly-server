0ab8e0d103eb4a44d078cfb11c5aeff3f03d893f
vladimirnovacek <novacek.vladimir@gmail.com>
2024-01-16 20:42:27 +0100

    Triple double support

--- state/end_turn.py
+++ state/end_turn.py
@@ -9,9 +9,12 @@ class EndTurnState(State):
         return set()
 
     def parse(self, message: ClientMessage):
+        # TODO: If player in jail, new state is supposed to be InJailState or something like that
+        # TODO: If player in bankrupt, new state is supposed to be BankruptState or something like that
         game_data = self.controller.game_data
         if message["action"] == "end_turn" and game_data.is_player_on_turn(message["my_uuid"]):
             from state.begin_turn import BeginTurnState
             game_data.update(section="misc", item="on_turn", value=next(game_data.player_order_cycler))
+            self.controller.dice.reset()
             self._change_state(BeginTurnState(self.controller))
             self._broadcast_changes()
