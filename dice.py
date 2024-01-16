4e239733731795603b45d809730bb2e9a36d0922
vladimirnovacek <novacek.vladimir@gmail.com>
2024-01-16 20:42:13 +0100

    Triple double support

--- dice.py
+++ dice.py
@@ -42,13 +42,35 @@ class Dice:
 
     def __init__(self, count: int = 2, sides: int = 6):
         self.last_roll: Roll | None = None
-        self.count = count
-        self.sides = sides
+        """ The last roll. None if no roll has been made yet. """
+        self.count: int = count
+        """ The number of dice. """
+        self.sides: int = sides
+        """ The number of sides of the dice. """
+        self.doubles: int = 0
+        """ The count of doubles in the row. """
+
+    @property
+    def triple_double(self) -> bool:
+        """
+        Returns True if three doubles in a row.
+        :return: True if three doubles in a row.
+        :rtype: bool
+        """
+        return self.doubles >= 3
+
+    def reset(self) -> None:
+        """
+        Resets the dice.
+        :return:
+        :rtype:
+        """
+        self.doubles = 0
 
     def roll(self, register=True) -> Roll:
         """
         Rolls the dice.
-        :param register: If False, the roll will not be accessible via last_roll attribute.
+        :param register: If True, the roll will be stored in the last_roll attribute and counts toward doubles.
         :type register: bool
         :return: Roll object
         :rtype: Roll
@@ -56,4 +78,8 @@ class Dice:
         roll = Roll(self.count, self.sides)
         if register:
             self.last_roll = roll
+            if roll.is_double():
+                self.doubles += 1
+            else:
+                self.doubles = 0
         return roll
