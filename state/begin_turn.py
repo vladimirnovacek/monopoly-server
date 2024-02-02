de04d9a730d9f14f2c3d0a93be04c7c1411927c7
vladimirnovacek <novacek.vladimir@gmail.com>
2024-02-02 22:25:27 +0100

    Implemented all stages except auction. Not tested

--- state/begin_turn.py
+++ state/begin_turn.py
@@ -1,6 +1,6 @@
 
 from board_description import FieldType
-from interfaces import ClientMessage, IPlayer, IField, IController
+from interfaces import ClientMessage, IPlayer, IField, IController, IRoll
 from state.state import State
 from state.end_turn import EndTurnState
 
@@ -9,6 +9,7 @@ class BeginTurnState(State):
     def __init__(self, controller: IController):
         super().__init__(controller)
         self.on_turn_player: IPlayer = self.controller.gd.on_turn_player
+        self.extra_roll: IRoll | None = None
 
     @property
     def on_turn_player_field(self) -> IField:
@@ -38,9 +39,12 @@ class BeginTurnState(State):
         elif self.stage == "buying_decision":
             match message["action"]:
                 case "buy":
-                    self.stage = "buying_property"  # TODO
+                    self.stage = "buying_property"
                 case "auction":
-                    self.stage = "auctioning"  # TODO
+                    self.stage = "auctioning"
+        elif self.stage == "rent_roll":
+            if message["action"] == "roll":
+                self.stage = "rent_rolling"
         else:
             self.stage = "rolling"
         self.input_expected = False
@@ -48,18 +52,28 @@ class BeginTurnState(State):
             match self.stage:
                 case "rolling":
                     self.stage = self._roll_dice()
+                case "rent_rolling":
+                    self.stage = self._rent_roll()
                 case "triple_double":
                     self.stage = self._go_to_jail()
                 case "moving":
                     self.stage = self._move()
+                case "moved":
+                    self.stage = self._moved()
                 case "on_property":
                     self.stage = self._on_property()
+                case "on_card":
+                    self.stage = self._take_card()
                 case "pay_tax":
                     self.stage = self._pay_tax()
                 case "pay_rent":
                     self.stage = self._pay_rent()
                 case "unowned_property":
                     self.stage = self._unowned_property()
+                case "buying_property":
+                    self.stage = self._buy_property()
+                case "auctioning":
+                    self.stage = "end_roll"  # TODO
                 case "go_to_jail":
                     self.stage = self._go_to_jail()
                 case "roll_in_jail":
@@ -78,12 +92,19 @@ class BeginTurnState(State):
         else:
             return "moving"
 
+    def _rent_roll(self) -> str:
+        self.controller.roll(False)
+        return "pay_rent"
+
     def _go_to_jail(self) -> str:
         self.controller.move_to(self.controller.gd.fields.JAIL)
         return "end_turn"
 
     def _move(self) -> str:
         self.controller.move_by(self.controller.dice.last_roll.sum())
+        return "moved"
+
+    def _moved(self) -> str:
         match self.on_turn_player_field.type:
             case FieldType.GO | FieldType.JUST_VISITING | FieldType.FREE_PARKING:
                 return "end_roll"
@@ -92,7 +113,7 @@ class BeginTurnState(State):
             case FieldType.TAX:
                 return "pay_tax"
             case FieldType.CC | FieldType.CHANCE:
-                return "on_card"  # TODO
+                return "on_card"
             case FieldType.GO_TO_JAIL:
                 return "go_to_jail"
 
@@ -116,7 +137,21 @@ class BeginTurnState(State):
         rent = self.on_turn_player_field.rent
         if self.on_turn_player_field.type == FieldType.UTILITY:
             rent *= self.controller.dice.last_roll.sum()
+        if self.special_rent == "double":
+            rent *= 2
+        if self.special_rent == "10xroll":
+            if self.extra_roll:
+                rent = self.extra_roll.sum() * 10
+            else:
+                self.input_expected = True
+                return "extra_roll"
+        self.special_rent = ""
         self.controller.pay(rent, self.on_turn_player.player_uuid, self.on_turn_player_field.owner)
+        return "end_roll"
+
+    def _buy_property(self) -> str:
+        self.controller.pay(self.on_turn_player_field.price, self.on_turn_player.player_uuid)
+        self.on_turn_player_field.owner = self.on_turn_player.player_uuid
         return "end_roll"
 
     def _roll_in_jail(self) -> str:
@@ -127,47 +162,26 @@ class BeginTurnState(State):
             self.controller.gd.on_turn_player.jail_turns += 1
             return "end_turn"
 
+    def _take_card(self) -> str:
+        deck = self.controller.cc if self.on_turn_player_field.type == FieldType.CC else self.controller.chance
+        card = deck.draw()
+        card.apply(self.controller)
+        if card.special_rent:
+            self.special_rent = card.special_rent
+        if card.type == "move":
+            return "moved"
+        elif card.type == "go_to_jail":
+            return "go_to_jail"
+        elif card.ends_turn:
+            return "end_turn"
+        else:
+            return "end_roll"
+
     def _end_roll(self) -> str:
         if self.controller.dice.last_roll.is_double():
             return "rolling"
         else:
             return "end_turn"
-
-
-    # def _field_action(self, field_id):
-    #     game_data = self.controller.gd
-    #     field = game_data.fields.get_field(field_id)
-    #     match field.type:
-    #         case FieldType.JAIL:
-    #             logging.warning("This should never happen. Player is not supposed to get to jail by moving.")
-    #         case FieldType.GO | FieldType.JUST_VISITING | FieldType.FREE_PARKING:
-    #             self._actions_over()
-    #         case FieldType.GO_TO_JAIL:
-    #             self._move_to_jail()
-    #         case FieldType.RAILROAD | FieldType.STREET:
-    #             if field.owner:
-    #                 self._pay_rent(field)
-    #             else:
-    #                 self._change_state(BuyPropertyState(self.controller))
-    #         case FieldType.UTILITY:
-    #             if field.owner:
-    #                 self._pay_rent(field, multiplier=self.controller.dice.last_roll.sum())
-    #             else:
-    #                 self._change_state(BuyPropertyState(self.controller))
-    #         case FieldType.CC | FieldType.CHANCE:
-    #             self._draw_card()
-    #         case FieldType.TAX:
-    #             self._pay_tax(field)
-
-    # def _pay_rent(self, field: IField, multiplier: int = 1):
-    #     self.controller.pay(field.rent, self.on_turn_player.player_uuid, field.owner)
-    #     self._actions_over()
-    #
-    # def _actions_over(self):
-    #     if self.controller.dice.last_roll.is_double():
-    #         self._change_state(BeginTurnState(self.controller))
-    #     else:
-    #         self._end_turn()
 
     def _leave_jail(self):
         self.on_turn_player.in_jail = False
@@ -182,10 +196,35 @@ class BeginTurnState(State):
         if self.controller.gd.on_turn_player.jail_turns < 3:
             actions.add("roll")
         return actions
-    #
-    # def _draw_card(self):
-    #     pass
 
     def _end_turn(self):
         self._change_state(EndTurnState(self.controller))
         self._broadcast_changes()
+
+# === STAGES LIST ===
+'''
+stages = {
+    "auctioning",  # player decided not to buy a property
+    "buying_property",  # player decided to buy a property
+    "end_roll",  # player did all actions after roll. If player rolled doubles,
+                 # rolls again
+    "end_turn",  # player's turn is over
+    "go_to_jail",  # player is moving to jail
+    "leaving_jail",  # player is leaving jail
+    "moved",  # player moved to a new field. It is separated from "moving"
+              # because cards can move the player
+    "moving",  # player is moving to a new field
+    "on_card",  # player landed on a card field
+    "on_property",  # player landed on a property field
+    "pay_rent",  # player is going to pay a rent
+    "pay_tax",  # player is going to pay a tax
+    "payout",  # player decided to pay the fine to get out of jail
+    "rent_roll",  # special roll required. Induced by chance card for paying 10x roll
+                  # rent on utility field
+    "rent_rolling",  # player is rolling for the "rent_roll"(see above)
+    "roll_in_jail",  # player in jail decided to roll
+    "rolling",  # player is rolling for regular movement
+    "unowned_property",  # player landed on an unowned property
+    "use_card",  # player in jail decided to use a free of jail card
+    "triple_double",  # player rolled 3 doubles in a row
+} '''
